ce459d19b8584cf78159e41e7d436c8e887891cc
dd482IT <dd482@njit.edu>
2022-10-27 01:02:59 -0400

    Maven check and other small change

--- auto-builder/auto-compiler.sh
+++ auto-builder/auto-compiler.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-#This script is to compile a collection of github repo links to check if they are compilable. 
+# This script is to compile a collection of github repo links to check if they are compilable. 
+# First argument is the file containing a link to each repo you want to test build
+# Usage: ./auto-compiler.sh myfile
 
 #To do:
 #-Check the cloned directory recursively for a "build.gradle" 
@@ -9,15 +11,15 @@
 #-Modify script to use pop instead of cd
 
 file=$1 #file argument
-
+echo ["FLAAAAAAAAAAAAAAAAG"]
 if ! command -v git &> /dev/null
 then
     echo "git could not be found"
     exit 
 fi 
-
+echo ["FLAAAAAAAAAAAAAAAAG"]
 #check if argument file exists
-[ -f "$file" ] || echo "Usage: file.txt" || exit 0;
+[ -f "$file" ] || echo "Usage: file.txt"
 #make an ouput dir 
 mkdir -p output
 rootpath=$(pwd)
@@ -26,23 +28,37 @@ rm -rf "$outdir"/*
 
 #Loop through each link: May cause error if wrapped with "" 
 while read -r repo; do
-    
+
     dirName="$(basename -- "$repo")"
-    cd "$outdir" || exit 0
+    cd "$outdir" || echo "[-------No Out Dir-------]"
     mkdir "$outdir"/"$dirName"
-    echo "[-------------Cloning $repo-------------]"
+    echo "[-------------Cloning ["$repo"]-------------]"
     git clone "$repo" "$dirName" 
-    cd "$dirName" || exit 0
-    if [ -f "build.gradle" ] 
+    cd "$dirName" || echo "[-------No Repo Dir-------]" 
+
+    if [ -f "gradlew" ];
     then
-        echo "[-------------Build file exists-------------]"
+        echo "[-------------Gradlew file Exists-------------]"
         #</dev/null   
         gradlew_output=$(./gradlew compileJava </dev/null 2>"$outdir"/"$dirName".log ); gradlew_return_code=$? 
         if [ $gradlew_return_code != 0 ] #test this on build that failes
         then 
             echo "[-------------Grade failed with exit status $gradlew_return_code-------------]"
             pwd
-            cd "$outdir" | exit 0
+            cd "$outdir" | exit 1
+            rm -rf "$dirName"
+            continue    
+        fi
+        echo "$repo" >> "$rootpath"/buildable.txt
+    elif [ -f "pom.xml" ];
+    then
+        echo "[-------------Pom.xml file Exists-------------]"
+        maven_output=$(mvn compile); maven_return_code=$?
+        if [ $maven_return_code != 0 ] #test this on build that failes
+        then 
+            echo "[-------------Maven failed with exit status $maven_return_code-------------]"
+            pwd
+            cd "$outdir" || exit 1
             rm -rf "$dirName"
             continue    
         fi
