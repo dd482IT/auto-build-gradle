e1e589d0489bc33f47c472dc73f700f5caed6362
dd482IT <dd482@njit.edu>
2022-11-07 14:53:08 -0500

    Full paths, more stats, fixed buildable.txt

--- auto-builder/auto-compiler.sh
+++ auto-builder/auto-compiler.sh
@@ -6,29 +6,38 @@
 # This works on gradle, maven and (still needs testing) ant projects. 
 # Usage: ./auto-compiler.sh myfile
 
-
+# check if git is installed
 if ! command -v git &> /dev/null
 then
     echo "git could not be found"
     exit 0
 fi 
 
-if [ -z "${1}" ]; then echo "File is required" && exit 0;fi
-file=$1 #file argument
+# debugging: time tracking
 
+start=`date +%s`
+
+
+# check if file was passed
+if [ -z "${1}" ]; then echo "File is required" && exit 0;fi
+file=$(readlink -f $1) #file argument
+
+# Delete previous build file if present
+if [ -f "buildable.txt" ]; 
+then 
+    rm buildable.txt
+fi
+# Create new text list
+touch buildable.txt
+buildable=$(readlink -f buildable.txt)
+# Staistics variables
 linksCount=$(cat "$file" | wc -l)
 iteration=0
 successful=0 
 failed=0
-
-
-#check if argument file exists
-[ -f "$file" ] || echo "Usage: file.txt"
-#make an ouput dir 
-mkdir -p output
-rootpath=$(pwd)
-outdir=$(realpath output)
-rm -rf "$outdir"/*
+missingBuildFile=0
+#make an ouput dir to hold the cloned repos
+mkdir -p output && cd output && outdir=$(pwd)
 
 #Loop through each link: May cause error if wrapped with 
 while read -r repo; do
@@ -36,19 +45,17 @@ while read -r repo; do
 # Extract name of project
     dirName="$(basename -- "$repo")"
 # Change into workspace and make project directory
-    cd "$outdir" 
+    cd $outdir
     mkdir "$dirName"
 # Begin cloning project
     git clone "$repo" "$dirName"
 # Change directory into project 
 # Check for build exec or just build
-    touch buildable.txt
     cd "$dirName" 
     iteration=$((iteration+1))
     if [ -f "gradlew" ];
     then
         echo "[Gradlew file Exists]"
-        #</dev/null   
         gradlew_output=$(./gradlew compileJava </dev/null 2>"$outdir"/"$dirName".log ); gradlew_return_code=$? 
         if [ $gradlew_return_code != 0 ] #test this on build that failes
         then 
@@ -58,7 +65,7 @@ while read -r repo; do
             rm -rf "$dirName"
             continue 
         fi
-        echo "$repo" >> buildable.txt
+        echo "$repo" >> $buildable
         successful=$((successful+1))
     elif [ -f "pom.xml" ];
     then
@@ -72,7 +79,7 @@ while read -r repo; do
             rm -rf "$dirName"
             continue    
         fi
-        echo "$repo" >> buildable.txt
+        echo "$repo" >> $buildable
         successful=$((successful+1))
     elif [ -f "build.xml" ];
     then
@@ -86,16 +93,19 @@ while read -r repo; do
             rm -rf "$dirName"
             continue    
         fi
-        echo "$repo" >> buildable.txt
+        echo "$repo" >> $buildable
         successful=$((successful+1))
     else
         echo "[Build file does not exist]"
         cd "$outdir" | exit
         echo "[Attempting to remove clone]"
         rm -rf "$dirName" && echo "[Deleted $dirName]"
-        failed=$((failed+1))
+        missingBuildFile=$((missingBuildFile+1))
     fi
     echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed]"
     #exit 0 # This is for testing a single project at a time. Remove this to use entire file.   
 done < "$file"
-echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed]"
+end=`date +%s`
+runtime=$((end-start))
+rm -r $outdir
+echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed Missing Build:$missingBuildFile Time:$runtime]"
