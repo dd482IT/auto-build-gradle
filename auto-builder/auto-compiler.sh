6865a2a1cbd97e3388ca27ce0c49c88131aa7df9
dd482IT <dd482@njit.edu>
2022-11-26 20:37:49 -0500

    Delete All projects; removed some output

--- auto-builder/auto-compiler.sh
+++ auto-builder/auto-compiler.sh
@@ -14,10 +14,6 @@ then
 fi 
 
 # debugging: time tracking
-
-start=`date +%s`
-
-
 # check if file was passed
 if [ -z "${1}" ]; then echo "File is required" && exit 0;fi
 file=$(readlink -f $1) #file argument
@@ -38,7 +34,6 @@ failed=0
 missingBuildFile=0
 #make an ouput dir to hold the cloned repos
 mkdir -p output && cd output && outdir=$(pwd)
-
 #Loop through each link: May cause error if wrapped with 
 while read -r repo; do
     echo "[Current Link $repo]"
@@ -59,53 +54,49 @@ while read -r repo; do
         gradlew_output=$(./gradlew compileJava </dev/null 2>"$outdir"/"$dirName".log ); gradlew_return_code=$? 
         if [ $gradlew_return_code != 0 ] #test this on build that failes
         then 
-            echo "[Grade failed with exit status $gradlew_return_code]"
             failed=$((failed+1))
             cd "$outdir" | exit 1
-            rm -rf "$dirName"
-            continue 
+        else 
+            echo "$repo" >> $buildable
+            successful=$((successful+1))
         fi
-        echo "$repo" >> $buildable
-        successful=$((successful+1))
+        rm -rf "$dirName"
+        continue 
     elif [ -f "pom.xml" ];
     then
         echo "[Pom.xml file Exists]"
         maven_output=$(mvn compile); maven_return_code=$?
         if [ $maven_return_code != 0 ] #test this on build that failes
         then 
-            echo "[Maven failed with exit status $maven_return_code]"
             failed=$((failed+1))
             cd "$outdir" || exit 1
-            rm -rf "$dirName"
-            continue    
+        else
+            echo "$repo" >> $buildable
+            successful=$((successful+1))    
         fi
-        echo "$repo" >> $buildable
-        successful=$((successful+1))
+        rm -rf "$dirName"
+        continue
     elif [ -f "build.xml" ];
     then
         echo "[Build.xml file Exists]"
         maven_output=$(ant compile); ant_return_code=$?
         if [ $ant_return_code != 0 ] #test this on build that failes
         then 
-            echo "[Ant failed with exit status $ant_return_code]"
             failed=$((failed+1))
             cd "$outdir" || exit 1
-            rm -rf "$dirName"
-            continue    
+        else 
+            echo "$repo" >> $buildable
+            successful=$((successful+1))
         fi
-        echo "$repo" >> $buildable
-        successful=$((successful+1))
+        rm -rf "$dirName"
+        continue
     else
-        echo "[Build file does not exist]"
         cd "$outdir" | exit
-        echo "[Attempting to remove clone]"
         rm -rf "$dirName" && echo "[Deleted $dirName]"
         missingBuildFile=$((missingBuildFile+1))
     fi
     echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed]"
     #exit 0 # This is for testing a single project at a time. Remove this to use entire file.   
 done < "$file"
-end=`date +%s`
-runtime=$((end-start))
 rm -r $outdir
-echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed Missing Build:$missingBuildFile Time:$runtime]"
+echo "[Progress:$iteration/$linksCount successful:$successful failed:$failed Missing Build:$missingBuildFile]"
